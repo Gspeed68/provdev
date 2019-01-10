0bb3591eae8d99ebf77a245bbe7fa024f62d960b
Gspeed68 <46546554+Gspeed68@users.noreply.github.com>
2019-01-09 20:17:12 -0500

    Add files via upload
    My first project.

---
+++ gregs_first_project.py
@@ -0,0 +1,30 @@
+print("-------------------------------------------------------")
+
+satisfaction = ("good")
+
+print("Hello")
+
+name = input("what is your name? ")
+
+feeling = input("How are you today, {}?  ".format(name))
+feeling = feeling.lower()
+
+# if feeling == 3:
+    # print("GOOD")
+# elif feeling > 3:
+print("So, " + name + ", you are feeling " + feeling + "?")
+
+print ("That is ." + feeling)
+
+
+
+
+
+print("-------------------------------------------------------")
+
+
+
+
+
+
+
